ee111a003e41bddbfdd1354fe6c1bdabad260ddd
Marcos-Antunes04 <mca4729@gmail.com>
2023-11-10 08:55:06 -0300

    Added first version timeline

--- to_do_list/src/views/Linha_do_tempo.js
+++ to_do_list/src/views/Linha_do_tempo.js
@@ -1,130 +1,153 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity, Image, Alert, Modal, Pressable } from 'react-native'
+// Calendar.js
 
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Button } from 'react-native';
+import Tarefas from './Tarefas.js';
 
+const Calendar = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
+  const handleDatePress = (date) => {
+    setSelectedDate(date);
+  };
 
-const Linha = (props) => {
+  const handlePrevMonthPress = () => {
+    setCurrentMonth((prevMonth) => (prevMonth - 1 + 12) % 12);
+    if (currentMonth - 1 < 0) {
+      setCurrentYear((prevYear) => prevYear - 1);
+    }
+  };
+
+  const handleNextMonthPress = () => {
+    setCurrentMonth((prevMonth) => (prevMonth + 1) % 12);
+    if (currentMonth + 1 > 11) {
+      setCurrentYear((prevYear) => prevYear + 1);
+    }
+  };
+
+  const renderDays = () => {
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const firstDayIndex = new Date(currentYear, currentMonth, 1).getDay();
+    const daysArray = [];
+
+    for (let i = 1; i <= daysInMonth + firstDayIndex; i++) {
+      const dayNumber = i - firstDayIndex;
+      const currentDate = new Date(currentYear, currentMonth, dayNumber);
+      const isToday = isSameDay(currentDate, new Date());
+
+      daysArray.push(
+        <TouchableOpacity
+          key={i}
+          style={[
+            styles.dayButton,
+            {
+              backgroundColor: currentDate.getMonth() === currentMonth ? (isToday ? '#ff7f50' : '#ffffff') : '#ff7f50',
+              opacity: dayNumber > 0 ? 1 : 0,
+            },
+          ]}
+          onPress={() => handleDatePress(currentDate)}
+        >
+          <Text style={{ color: isToday ? 'white' : '#333' }}>{dayNumber > 0 ? dayNumber : ''}</Text>
+        </TouchableOpacity>
+      );
+    }
+
+    return daysArray;
+  };
+
+  const isSameDay = (date1, date2) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
 
   return (
-    <ScrollView style={{flex:1, backgroundColor:'blue'}}>
-     <Text>calendário</Text>
-    </ScrollView>
-  )
-}
+    
+    <View style={styles.container}>
+      <Text style={styles.timelineText}>Linha do Tempo</Text>
+      <View style={styles.calendarHeader}>
+        <Button title={`${getAbbrMonthName(currentMonth - 1)} ${currentYear}`} onPress={handlePrevMonthPress} />
+        <Text style={[styles.monthYearText, styles.monthYearContainer]}>{`${getAbbrMonthName(currentMonth)} ${currentYear}`}</Text>
+        <Button title={`${getAbbrMonthName(currentMonth + 1)} ${currentYear}`} onPress={handleNextMonthPress} />
+      </View>
+      <View style={styles.calendarContainer}>{renderDays()}</View>
+      {selectedDate && (
+        <View style={styles.selectedDateContainer}>
+          <Text style={[styles.selectedDateText, { color: 'white' }]}>{selectedDate.toDateString()}</Text>
+          
+        </View>
+      )}
+    </View>
+  );
+};
 
-export default Linha
+const getAbbrMonthName = (monthIndex) => {
+  const monthNames = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+  return monthNames[(monthIndex + 12) % 12];
+};
 
 const styles = StyleSheet.create({
-  topo:{
-    height:200,
-    backgroundColor:'#e16400',
-    borderBottomLeftRadius:300,
-    borderBottomRightRadius:250,
-    flex:1
-  },
-  input:{
-    backgroundColor: 'white',
-    marginLeft:50,
-    marginTop:100,
-    borderTopLeftRadius:25,
-    borderBottomLeftRadius:25,
-    height:28,
-    paddingTop:4,
-    paddingLeft:8,
-    flex:5
-  },
-  input2:{
-    marginRight:50,
-    marginTop:100,
-    borderTopRightRadius:25,
-    borderBottomRightRadius:25,
-    height:28,
-    paddingTop:4,
-    paddingLeft:8,
-    flex:1,
-    backgroundColor:'black'
-  },
-  scan:{
-    flexDirection:'row',
-    paddingLeft:30,
-    flex:1,
+  container: {
     alignItems: 'center',
-    alignContent:'center',
-    height:150,
-    width:'90%',
-    borderRadius:30,
-    backgroundColor:'white',
-    alignSelf:'center',
-    zIndex:1,
-    shadowColor: '#a1a1a1',
-    shadowOffset: {width: 0, height: 1},
-    shadowOpacity: 0.7,
-    elevation: 1
-  },
-  mapButton:{
-    marginTop:15,
-    marginLeft:'35%',   
-    flex:1
-  },
-  texto:{
-    alignSelf:'center',
-    marginTop:160,
-    textAlign: 'center',
-    fontSize:22,
-    flex:1
-  },
-  textoScan:{
-    padding:20,
-    textAlign:'center'
-  },
-  botoes:{
-    flex:1,
-    marginTop:25,
-    height:90,
-    justifyContent:'space-evenly',
-  },
-  centeredView: {
+    backgroundColor: '#4169E1',
     flex: 1,
-    alignItems: "center",
-    marginTop: 75,
-    justifyContent:'center'
+    paddingTop: 20,
   },
-  modalView: {
-    height:'80%',
-    width:'95%',
-    backgroundColor: "white",
-    borderRadius: 5,
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
+  timelineText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 10,
   },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    margin:10
+  calendarHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
   },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
+  calendarContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    borderRadius: 10,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: '#4169E1',
+    width: '90%',
+    marginTop: 10,
+    maxHeight: 300,
   },
-  buttonClose: {
-    width:'20%',
-    backgroundColor: "#e16400",
+  dayButton: {
+    width: '14.28%',
+    aspectRatio: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#4169E1',
   },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
+  monthYearText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
   },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  }
+  monthYearContainer: {
+    backgroundColor: '#ff7f50',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  selectedDateContainer: {
+    marginTop: 20,
+  },
+  selectedDateText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
+
+export default Calendar;
